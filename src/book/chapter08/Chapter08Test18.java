a589836d13f57c120f08ac2d5537afbefd6238e6
tryoo0607 <tryoo0607@gmail.com>
2025-05-26 20:35:11 +0900

    Chapter08 - Test18 추가

--- src/book/chapter08/Chapter08Test18.java
+++ src/book/chapter08/Chapter08Test18.java
@@ -2,6 +2,9 @@ package book.chapter08;
 
 import common.CodingTest;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 // 문제 18. 두 개의 수로 특정 값 만들기 *
 public class Chapter08Test18 extends CodingTest {
 
@@ -18,7 +21,32 @@ public class Chapter08Test18 extends CodingTest {
         testCases.add(() -> createCase(ARR2, TARGET2));
     }
 
+    /*
+        시간 복잡도 : O(N)
+        공간 복잡도 : O(N)
+    */
     private void createCase(int[] arr, int target) {
 
+        int expected = arr.length;
+        float loadFactor = 0.75f;
+        int cap = (int)(expected / loadFactor) + 1;
+
+        HashSet<Integer> hashSet = new HashSet<>(cap);  // 공간복잡도 : O(N)
+
+        boolean answer = false;
+        for(int num : arr) {                            //  O(N)
+
+            if(hashSet.contains(target - num)) {        //  O(1)
+                answer = true;
+                break;
+            }
+
+            hashSet.add(num);                           // O(1)
+        }
+
+        System.out.println("[작업 이전] : ");
+        System.out.println("[arr] : " + Arrays.toString(arr));
+        System.out.println("[target] : " + target);
+        System.out.println("[작업 결과] : " + answer);
     }
 }
